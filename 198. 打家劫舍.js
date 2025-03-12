b60e0f80cd3876835dac67e7390d7c13bf1cdc81
anguske <anguske@aaxischina.com>
2025-03-12 14:23:59 +0800

    1

--- 198. 打家劫舍.js
+++ 198. 打家劫舍.js
@@ -29,12 +29,11 @@ var rob = function (nums) {
   const dp = [];
   dp[0] = nums[0];
   dp[1] = Math.max(nums[0], nums[1]);
-    for (let i = 2; i < nums.length; i++) {
+  for (let i = 2; i < nums.length; i++) {
     dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
   }
   return dp[nums.length - 1];
 };
-
 
 var rob = function (nums) {
   if (nums.length === 1) {
@@ -46,9 +45,9 @@ var rob = function (nums) {
   let prev1 = nums[0];
   let prev2 = Math.max(nums[0], nums[1]);
   for (let i = 2; i < nums.length; i++) {
-      const current = Math.max(prev2 + nums[i], prev1);
-      prev2 = prev1
-      prev1 =current
+    const current = Math.max(prev2 + nums[i], prev1);
+    prev2 = prev1;
+    prev1 = current;
   }
   return prev1;
 };
