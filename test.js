a1948e94a738605ec98537d89d5cd0970c418254
kepengqi <kepengqi@gmail.com>
2025-03-27 07:50:54 +0800

    在 test.js 中新增序列化和反序列化二叉树的函数，同时重构 invertTree 函数以使用栈代替队列，优化了树的节点交换逻辑。此外，添加了 flatten 和 classnames 函数的实现。

--- test.js
+++ test.js
@@ -1,22 +1,74 @@
-function invertTree(root) {
-  if (root === null) {
+function serialize(root) {
+  if (!root) {
+    return "-";
+  }
+  return `${root.val},${serialize(root.left)},${serialize(root.right)}`;
+}
+/**
+ * @param {string} str
+ * @return {Node}
+ */
+function deserialize(str) {
+  const queue = str.join(",");
+  if (!queue) {
     return null;
   }
-  const queue = [root]; // 使用队列存储节点
-  while (queue.length > 0) {
-    const node = queue.shift(); // 取出当前节点
-    // 交换左右子节点
-    const temp = node.left;
-    node.left = node.right;
-    node.right = temp;
-    // 将子节点加入队列
-    if (node.left !== null) {
-      queue.push(node.left);
+
+  const dps = (queue) => {
+    if (!queue.length) {
+      return null;
     }
-    if (node.right !== null) {
-      queue.push(node.right);
+    const node = queue.shift();
+    if (node !== "-") {
+      const newNode = new NodeList(node.val);
+      newNode.left = dps(queue);
+      newNode.right(dps(queue));
+      return newNode;
     }
-  }
+    return null;
+  };
+  const root = dps(queue);
   return root;
 }
 
+function invertTree(root) {
+  if (!root) {
+    return null;
+  }
+  const stack = [root];
+  while (stack.length) {
+    const node = stack.shift();
+    const temp = node.left;
+    node.left = node.right;
+    node.right = node.left;
+    stack.push(node.left);
+    stack.push(node.right);
+  }
+}
+
+function flatten(arr) {
+  arr.reduce((acc, item) => {
+    if (Array.isArray(item)) {
+      return acc.concat(flatten(item));
+    }
+    return acc.concat(item);
+  }, []);
+}
+function classnames(...args) {
+  const classes = [];
+  if (typeof args === "number" || typeof args === "string") {
+    classes.push(args);
+  }
+  if (Array.isArray(args)) {
+    args.forEach((item) => {
+      classes.push(classnames(item));
+    });
+  }
+  if (args instanceof Object) {
+    Reflect.ownKeys.forEach((key) => {
+      if (args[key]) {
+        classes.push(key);
+      }
+    });
+  }
+}
